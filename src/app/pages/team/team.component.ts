f72c6c7e24cdfdaac0af351e6d492fdd5fd689e8
Oriana Bautista <oribautista99@gmail.com>
2023-03-26 21:07:43 -0400

    Agregar imagenes

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -14,12 +14,12 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 export class TeamComponent {
 
   person: any[] = [
-    {sex: 'w', name: 'Angie Lorena Bautista Iscala', ci:'30.569.673', job: 'Especialista en marketing'},
-    {sex: 'w', name: 'Karen Andrea Carrera Herrera', ci:'30.569.673', job: 'Especialista en marketing'},
-    {sex: 'm', name: 'Luis Alexander García Arellano', ci:'30.569.673', job: 'Especialista en marketing'},
-    {sex: 'w', name: 'Mariangel Montilva Peñaloza', ci:'30.569.673', job: 'Especialista en marketing'},
-    {sex: 'm', name: 'Leandro Josue Zambrano Corredor', ci:'30.569.673', job: 'Especialista en marketing'},
-    {sex: 'm', name: 'Javier Eduardo Suarez Tapias', ci:'30.569.673', job: 'Especialista en marketing'},
+    {sex: 'w', name: 'Angie Lorena Bautista Iscala', ci:'30.569.673', job: 'angie.jpg'},
+    {sex: 'w', name: 'Karen Andrea Carrera Herrera', ci:'30.569.673', job: 'karen.jpg'},
+    {sex: 'm', name: 'Luis Alexander García Arellano', ci:'30.569.673'},
+    {sex: 'w', name: 'Mariangel Montilva Peñaloza', ci:'30.569.673'},
+    {sex: 'm', name: 'Leandro Josue Zambrano Corredor', ci:'30.569.673'},
+    {sex: 'm', name: 'Javier Eduardo Suarez Tapias', ci:'30.569.673'},
   ];
 
 }
