f3538ed81b057fdc025e310fb5e9196df3fefb67
Oriana Bautista <oribautista99@gmail.com>
2023-03-27 20:25:19 -0400

    Add picture update components

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -12,9 +12,9 @@ export class TeamComponent {
     {sex: 'w', name: 'Angie Lorena Bautista Iscala', ci:'30.569.673', job: 'angie.jpg'},
     {sex: 'w', name: 'Karen Andrea Carrera Herrera', ci:'27.567.551', job: 'karen.jpg'},
     {sex: 'm', name: 'Luis Alexander García Arellano', ci:'30.111.344', job:'luis.jpg'},
-    {sex: 'w', name: 'Mariangel Montilva Peñaloza', ci:'30.569.673', job:'mariangel.jpg'},
+    {sex: 'w', name: 'Mariangel Montilva Peñaloza', ci:'30.189.425', job:'mariangel.jpg'},
     {sex: 'm', name: 'Leandro Josue Zambrano Corredor', ci:'30.339.198', job:'leandro.jpg'},
-    {sex: 'm', name: 'Javier Eduardo Suarez Tapias', ci:'30.569.673', job:'javier.jpg'},
+    {sex: 'm', name: 'Javier Eduardo Suarez Tapias', ci:'27.124.166', job:'javier.jpg'},
   ];
 
 }
